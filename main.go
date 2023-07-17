e5bd9602fa0299daba91cf3dc7cd7a1ac0cb0d81
Rusisg <rasul.nulrybekov@gmail.com>
2023-07-17 22:55:02 +0500

    replace main work

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Rusisg/blockchain/blockchain"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	chain := blockchain.InitBlockchain()
+
+	chain.AddBlock("First block after Genesis")
+	chain.AddBlock("Second block after Genesis")
+	chain.AddBlock("Third block after Genesis")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println("")
+	}
 }
